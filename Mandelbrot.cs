0e7ab14b0c13bd2209e84b8c6c62a73216c5cfec
Thomasklaassen <thomasklaassen8@gmail.com>
2025-09-24 21:37:06 +0200

    Added error messages. Added a dropdown menu with standard colours. Enter button now also uses gobutton. Changes GoKnop click because it was not using any of the values from the input fields.

--- Mandelbrot.cs
+++ Mandelbrot.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -6,17 +7,19 @@ using System.Windows.Forms;
 
 
 
-
-
+//Form variable init
 double middenX = -0.5;
 double middenY = 0;
 double schaal = 0.01;
 int maxIteraties = 100;
 const int canvasGrootte = 500;
+
+
 //form init
 Form scherm = new Form();
 scherm.Text = "Mandelbrot";
-scherm.ClientSize = new Size(500, 600);
+scherm.ClientSize = new Size(500, 700);
+scherm.KeyPreview = true;
 
 //Bitmaps and picturebox
 Bitmap plaatje = new Bitmap(canvasGrootte, canvasGrootte);
@@ -50,17 +53,26 @@ TextBox schaalinvoer = new TextBox { Location = new Point(100, 70)};
 Label MaxIterationtekst = new Label { Text = "Max herhaling", AutoSize = true, Location = new Point(10, 100)};
 TextBox MaxIterationInvoer = new TextBox { Location = new Point(100, 100)};
 
-Button goKnop = new Button { Text = "Go!", Location = new Point(200, 50)};
+Label Errormsg = new Label { Text = "", ForeColor = Color.Red, AutoSize = true, Location = new Point(0, 150) };
+
+Button goKnop = new Button { Text = "Go!", Location = new Point(120, 150)};
+scherm.AcceptButton = goKnop; // use enter to click go button.
 
 
+
+//dropdown menu for selecting colour scheme.
+ComboBox kleurMenu = new ComboBox { Name = "Kleurenschema:", Location = new Point(200, 10), Size = new System.Drawing.Size(150, 50), DropDownStyle = ComboBoxStyle.DropDownList};
+kleurMenu.Items.AddRange( new object[] { "Zwart-wit", "Blauw", "Regenboog" });
+kleurMenu.SelectedItem = "Zwart-wit";
 
 //Create control list and add all controls to the canvas. 
-Control[] allControls = new Control[] { xtekst, invoerx, ytekst, invoery, schaaltekst, schaalinvoer, MaxIterationtekst, MaxIterationInvoer, goKnop };
+Control[] allControls = new Control[] { xtekst, invoerx, ytekst, invoery, schaaltekst, schaalinvoer, MaxIterationtekst, MaxIterationInvoer, goKnop, Errormsg, kleurMenu };
 controlPanel.Controls.AddRange(allControls);
 scherm.Controls.Add(canvas);
 scherm.Controls.Add(controlPanel);
 
 controlPanel.Height = 250;
+controlPanel.Width = 500;
 
 controlPanel.Top = canvas.Height;
 
@@ -90,19 +102,63 @@ int BerekenMandelgetal(double x, double y, int maxIteraties)
 
 Color BepaalKleur(int mandelgetal)
 {
-    if (mandelgetal == -1)
+    if(kleurMenu.SelectedIndex == 2) // regenboog
     {
-        return Color.Black;
+        if(mandelgetal == -1)
+        {
+            return Color.Black;
+        }
+        float b = mandelgetal % 10;
+        switch (b)
+        {
+            case 0: return Color.FromArgb(255, 0, 0);
+            case 1: return Color.FromArgb(204, 102, 0);
+            case 2: return Color.FromArgb(153, 204, 0);
+            case 3: return Color.FromArgb(51, 255, 0);
+            case 4: return Color.FromArgb(0, 255, 102);
+            case 5: return Color.FromArgb(0, 255, 204);
+            case 6: return Color.FromArgb(0, 204, 255);
+            case 7: return Color.FromArgb(0, 102, 255);
+            case 8: return Color.FromArgb(51, 0, 255);
+            case 9: return Color.FromArgb(102, 0, 255);
+            default: return Color.White;
+        }
+
     }
-    else if (mandelgetal % 2 == 0)
+    else if(kleurMenu.SelectedIndex == 1) // blauw
+    { 
+        if (mandelgetal == -1)
+        {
+            return Color.Black;
+        }
+        int value = (255 * mandelgetal / maxIteraties);
+        int b = Math.Max(value, 30);
+        return Color.FromArgb(0, 0, b);
+    }
+    else if (kleurMenu.SelectedIndex == 0)  // zwart wit. 
     {
-        return Color.Black;
+        if (mandelgetal == -1)
+        {
+            return Color.Black;
+        }
+        else if (mandelgetal % 2 == 0)
+        {
+            return Color.Black;
+        }
+        else
+        {
+            return Color.White;
+        }
     }
     else
     {
+        Errormsg.Text = "Selecteer een kleurschema.";
         return Color.White;
     }
+  
+
 }
+
 void TekenMandelbrot(double mx, double my, double s, int maxIter)
 {
     using (Graphics gr = Graphics.FromImage(plaatje))
@@ -129,14 +185,40 @@ void TekenMandelbrot(double mx, double my, double s, int maxIter)
 }
 void GoKnop_Click(object sender, EventArgs e)
 {
+    List<String> InvoerStrings = new List<String>() { invoerx.Text, invoery.Text, schaalinvoer.Text, MaxIterationInvoer.Text };
+    foreach(string s in InvoerStrings)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            Errormsg.Text = "Vul alle velden in!";
+            return;
+        }
+        else
+        {
+            Errormsg.Text = "";
+        }
+    }
     // lees de waarden uit de tekstvakken en update de variabelen
-    if (double.TryParse(invoerx.Text, out double newX)) middenX = newX;
-    if (double.TryParse(invoery.Text, out double newY)) middenY = newY;
-    if (double.TryParse(schaalinvoer.Text, out double newSchaal)) schaal = newSchaal;
-    if (int.TryParse(MaxIterationInvoer.Text, out int newMaxIteraties)) maxIteraties = newMaxIteraties;
+    try
+    {
+        middenX = double.Parse(invoerx.Text);
+        middenY = double.Parse(invoery.Text);
+        schaal = double.Parse(schaalinvoer.Text);
+        maxIteraties = int.Parse(MaxIterationInvoer.Text);
+    }
+    catch 
+    {
+        Errormsg.Text = "Vul alleen cijfers \n of comma's in!";
+        return;
+    }
+
 
     TekenMandelbrot(middenX, middenY, schaal, maxIteraties);
 }
+
+
+
+//Event handlers
 void canvas_MouseDown(object sender, MouseEventArgs e)
 {
     // bepaalt het nieuwe middelpunt van de muisklik
@@ -164,7 +246,6 @@ void canvas_MouseDown(object sender, MouseEventArgs e)
 
 goKnop.Click += GoKnop_Click;
 canvas.MouseDown += canvas_MouseDown;
-TekenMandelbrot(middenX, middenY, schaal, maxIteraties);
 
 
 
